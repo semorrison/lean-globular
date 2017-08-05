c0985ce06da98c8a2d1ce96362b00085fd35d6de
Scott Morrison <scott@tqft.net>
2017-08-05 21:07:57 +1000

    initial attempt

--- src/globular/signature.lean
+++ src/globular/signature.lean
@@ -4,4 +4,4 @@ with signature : ℕ → Type 2
 | s          : Π n : ℕ, Π g : Type, Π σ : (signature n), Π s t : g → diagram n σ, signature (n+1)
 with diagram : Π n : ℕ, Π σ : (signature n), Type 2
 | empty      : diagram 0
-| d          : Π n : ℕ, Π σ : (signature (n+1)), Π s : diagram n sorry, diagram (n+1) σ
+| d          : Π n : ℕ, Π σ : (signature (n+1)), Π s : diagram n sorry, Π δ : list sorry, diagram (n+1) σ
