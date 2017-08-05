fb221e0399aea58144c3f2a3c0d80ec7fdf42ceb
Scott Morrison <scott@tqft.net>
2017-08-06 00:02:40 +1000

    recursive definition of a signature and diagram, thanks to Mario Carneiro

--- src/globular/signature.lean
+++ src/globular/signature.lean
@@ -1,7 +1,17 @@
-mutual inductive signature, diagram
-with signature : ℕ → Type 2
-| empty      : signature 0
-| s          : Π n : ℕ, Π g : Type, Π σ : (signature n), Π s t : g → diagram n σ, signature (n+1)
-with diagram : Π n : ℕ, Π σ : (signature n), Type 2
-| empty      : diagram 0
-| d          : Π n : ℕ, Π σ : (signature (n+1)), Π s : diagram n sorry, Π δ : list sorry, diagram (n+1) σ
+structure {u} signature_data (S : Type (u+1)) (D : S → Type (u+1)) :=
+(g : Type u)
+(σ : S)
+(s t : g → D σ)
+
+structure unchecked_diagram_data (n : ℕ) (S D) (σ : signature_data S D) :=
+(s : D σ.σ)
+(δ : list (σ.g × vector nat n))
+
+definition {u} unchecked_signature_diagram_pair : ℕ → Σ T : Type (u+1), T → Type (u+1)
+| 0     := ⟨ulift unit, λ _, ulift unit⟩
+| (n+1) := let ⟨S, D⟩ := unchecked_signature_diagram_pair n in 
+             ⟨signature_data S D, unchecked_diagram_data n S D⟩
+
+definition {u} unchecked_signature (n : ℕ) : Type (u+1)                          := (unchecked_signature_diagram_pair n).1
+definition {u} unchecked_diagram   (n : ℕ) : unchecked_signature n → Type (u+1) := (unchecked_signature_diagram_pair n).2
+
